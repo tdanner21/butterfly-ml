df25942afd554ace7ef949fa2700c23b753a9f20
Tyler Danner <tylerdanner@Tylers-MacBook-Air-96.local>
2026-07-03 20:29:18 -0400

    ResNet50 + targeted augmentation, ~84% val accuracy (Run 8 config)

--- src/train.py
+++ src/train.py
@@ -22,10 +22,12 @@ train_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
+    transforms.RandomResizedCrop(224, scale=(0.7, 1.0)),
     transforms.ColorJitter(brightness=0.2, contrast=0.2),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225])
+                        std=[0.229, 0.224, 0.225]),
+    transforms.RandomErasing(p=0.2)
 ])
 
 val_transforms = transforms.Compose([
